64ab247f396d456094e20839e0449ec0ee38f401
nearnshaw <nearnshaw@decentraland.org>
2021-02-12 14:16:02 -0300

    new utils lib version

--- src/ring.ts
+++ src/ring.ts
@@ -1,9 +1,8 @@
-import utils from "../node_modules/decentraland-ecs-utils/index"
-import { ToggleState } from "../node_modules/decentraland-ecs-utils/toggle/toggleComponent"
-import { Sound } from "./sound"
+import * as utils from '@dcl/ecs-scene-utils'
+import { Sound } from './sound'
 
 // Sound
-const ringPass = new Sound(new AudioClip("sounds/ringPass.mp3"), false)
+const ringPass = new Sound(new AudioClip('sounds/ringPass.mp3'), false)
 
 // Config
 const EDGE_OFFSET = 6
@@ -26,42 +25,62 @@ export class Ring extends Entity {
     this.endPos = new Vector3(startPos.x, startPos.y + Y_OFFSET, startPos.z)
 
     this.addComponent(
-      new utils.TriggerComponent(new utils.TriggerBoxShape(new Vector3(10, 8.5, 1), new Vector3(0, 1.2, 0)),
-        null, null, null, null, null, () => {
-        // Randomly reposition the ring after player passes through the ring
-        this.startPos = new Vector3(
-          Math.random() * SCENE_SIZE + EDGE_OFFSET,
-          Math.random() * MAX_HEIGHT + GROUND_OFFSET,
-          Math.random() * SCENE_SIZE + EDGE_OFFSET
-        )
-        this.endPos = new Vector3(this.startPos.x, this.startPos.y + GROUND_OFFSET, this.startPos.z)
-        ringPass.getComponent(AudioSource).playOnce()
-      })
+      new utils.TriggerComponent(
+        new utils.TriggerBoxShape(
+          new Vector3(10, 8.5, 1),
+          new Vector3(0, 1.2, 0)
+        ),
+        {
+          onCameraExit: () => {
+            // Randomly reposition the ring after player passes through the ring
+            this.startPos = new Vector3(
+              Math.random() * SCENE_SIZE + EDGE_OFFSET,
+              Math.random() * MAX_HEIGHT + GROUND_OFFSET,
+              Math.random() * SCENE_SIZE + EDGE_OFFSET
+            )
+            this.endPos = new Vector3(
+              this.startPos.x,
+              this.startPos.y + GROUND_OFFSET,
+              this.startPos.z
+            )
+            ringPass.getComponent(AudioSource).playOnce()
+          },
+        }
+      )
     )
 
     // Move the ring up and down between start and end positions
     this.addComponent(
-      new utils.ToggleComponent(utils.ToggleState.Off, (value: ToggleState) => {
-        if (value == utils.ToggleState.On) {
-          this.addComponentOrReplace(
-            new utils.MoveTransformComponent(this.startPos, this.endPos, time,
-              () => {
-                this.getComponent(utils.ToggleComponent).toggle()
-              },
-              utils.InterpolationType.EASEQUAD // Ease in and out
+      new utils.ToggleComponent(
+        utils.ToggleState.Off,
+        (value: utils.ToggleState) => {
+          if (value == utils.ToggleState.On) {
+            this.addComponentOrReplace(
+              new utils.MoveTransformComponent(
+                this.startPos,
+                this.endPos,
+                time,
+                () => {
+                  this.getComponent(utils.ToggleComponent).toggle()
+                },
+                utils.InterpolationType.EASEQUAD // Ease in and out
+              )
             )
-          )
-        } else {
-          this.addComponentOrReplace(
-            new utils.MoveTransformComponent(this.endPos, this.startPos, time,
-              () => {
-                this.getComponent(utils.ToggleComponent).toggle()
-              },
-              utils.InterpolationType.EASEQUAD // Ease in and out
+          } else {
+            this.addComponentOrReplace(
+              new utils.MoveTransformComponent(
+                this.endPos,
+                this.startPos,
+                time,
+                () => {
+                  this.getComponent(utils.ToggleComponent).toggle()
+                },
+                utils.InterpolationType.EASEQUAD // Ease in and out
+              )
             )
-          )
+          }
         }
-      })
+      )
     )
     this.getComponent(utils.ToggleComponent).toggle()
   }
